7e2b8d393134fb97ffc6b97f52ce01f76b92280d
Mohammod Ashikur Rahman <122194673+ashikbuilds@users.noreply.github.com>
2026-07-26 01:43:21 +0600

    Add files via upload

--- HTTP methods in FastAPI/main.py
+++ HTTP methods in FastAPI/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
 
+@app.get("/")
+def hello():
+    return {'message':'Patients Managemet System API'}
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    return data
+
+@app.get("/about")
+def about():
+    return {'message':'A fully functional API to manage my Patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data

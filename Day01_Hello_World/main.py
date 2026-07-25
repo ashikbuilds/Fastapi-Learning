7c65535ec337b058d7a14f1cf4b39f66dffc53b8
Mohammod Ashikur Rahman <122194673+ashikbuilds@users.noreply.github.com>
2026-07-26 00:53:04 +0600

    Add files via upload

--- Day01_Hello_World/main.py
+++ Day01_Hello_World/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {'message':'Hello World'}
+
+@app.get("/about")
+def about():
+    return {'message':'Ashik First time Apply FASTAPI'}

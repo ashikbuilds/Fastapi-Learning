8813d3edd934d00326bdba0332f1c67f99c9f7de
Mohammod Ashikur Rahman <122194673+ashikbuilds@users.noreply.github.com>
2026-08-01 14:40:21 +0600

    Add files via upload

---
+++ pydantic_crash/Data_validation.py
@@ -0,0 +1,19 @@
+def insert_patient_data(name: str, age: int):
+    if type(name) == str and type(age)== int:
+        if age<0:
+            raise ValueError('Age cant be negative')
+        else:
+            print(name)
+            print(age)
+            print('Inserted into database')
+    else:
+        raise TypeError('Incorrect data Type')
+
+def update_patient_data(name:str, age:int):
+    if type(name)== str and type(age)==int:
+        print(name)
+        print(age)
+        print('update')
+    else:
+        raise TypeError('Incorrect data type')
+insert_patient_data('Ashik',-10)
